e9a7c1d06f4bb07766aaf6542072ee9eebafa030
Bien Nguyen <nvbien2000@gmail.com>
2023-07-09 22:44:03 +0700

    first commit

---
+++ test/tree_view_flutter_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:tree_view_flutter/tree_view_flutter.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
